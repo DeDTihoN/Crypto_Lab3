a383bdce2df96e06d7da2a7ca4ebc0c9685bba37
DeDTihoN <t.grishechkin@gmail.com>
2024-05-23 16:46:18 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Crypto_Lab1_;
+using System;
+using System.Numerics;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+public class Program
+{
+    public static List<RSA> rsaList = new List<RSA>();
+    public static void Main()
+    {
+
+        RSA rsaBob = new RSA("Bob");
+        RSA rsaAlice = new RSA("Alice");
+
+        rsaList.Add(rsaBob);
+        rsaList.Add(rsaAlice);
+
+        while(true){
+            int sender = 0;
+
+            Console.WriteLine("Choose sender: 0 - {0}, 1 - {1}", rsaList[0].senderName, rsaList[1].senderName);
+            sender = Convert.ToInt32(Console.ReadLine());
+
+            if (!sender.Equals(0) && !sender.Equals(1)){
+                Console.WriteLine("Wrong number");
+                continue;
+            }
+
+            int receiver = 1 - sender;
+
+            Console.WriteLine("Enter message:");
+
+            string message = Console.ReadLine();
+
+            SentMessage(sender, receiver, message);
+        }
+    }
+
+    static void SentMessage(int sender, int receiver, string message){
+        if (rsaList[sender].checkMessageSupported(message) == false){
+            Console.WriteLine("Message is not supported by rsa");
+            return;
+        }
+
+        Console.WriteLine("{0} sent message \"{1}\", to {2}", rsaList[sender].senderName, message, rsaList[receiver].senderName);
+
+        string EncryptedMessage = rsaList[sender].EncryptMessage(message);
+
+        string DecryptedMessage = RSA.DecryptMessage(EncryptedMessage, rsaList[sender].e, rsaList[sender].n);
+
+        Console.WriteLine("Decrypted message: \n{0}", DecryptedMessage);
+    }
+
+    static string ToBase2(BigInteger number)
+    {
+        return number.ToString("B");
+    }
+
+    static string ToBase64(BigInteger number)
+    {
+        byte[] bytes = number.ToByteArray();
+        return Convert.ToBase64String(bytes);
+    }
+
+    static string ToByteArray(BigInteger number)
+    {
+        byte[] byteArray = number.ToByteArray();
+        string res = "";
+        res += "ByteArray: [";
+        for (int i = 0; i < byteArray.Length; i++)
+        {
+            res += byteArray[i].ToString("X");
+            if (i < byteArray.Length - 1)
+            {
+                res += ", ";
+            }
+        }
+        res += "]";
+        return res;
+    }
+}
---
+++ RSA.cs
@@ -0,0 +1,148 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Net.Security;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crypto_Lab1_
+{
+    public class RSA
+    {
+        public string senderName { get; set; } = "noname";
+        public BigInteger n { get; set; }
+        public BigInteger e { get; set; }
+        public BigInteger d { get; set; }
+        public BigInteger p { get; set; }
+        public BigInteger q { get; set; }
+        public BigInteger carmichaelPhi { get; set; }
+        public BigInteger dmodpsub1 { get; set; }
+        public BigInteger dmodqsub1 { get; set; }
+        const int PRIMES_LENGTH_DEFAULT = 200;
+        public int PRIMES_LENGTH { get; set; }
+
+        static public string supportedSymbols = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890:;~!@#$%^&*()_+-=[]{}\\.\',\"<>/?|₴№\r\t";
+
+        public bool checkMessageSupported(string message){
+            return message.All(x => supportedSymbols.Contains(x)) && ToInteger(message) < n;
+        }
+
+        public RSA(string senderName, int PRIMES_LENGTH = PRIMES_LENGTH_DEFAULT)
+        {
+            this.senderName = senderName;
+            this.PRIMES_LENGTH = PRIMES_LENGTH;
+            p = Ariphmetic.GeneratePrime(PRIMES_LENGTH);
+            q = Ariphmetic.GeneratePrime(PRIMES_LENGTH);
+            n = p * q;
+
+            carmichaelPhi = Ariphmetic.LCM(p - 1, q - 1);
+
+            e = Ariphmetic.CoPrime(carmichaelPhi);
+
+            d = Ariphmetic.GetInverseNumber(e, carmichaelPhi);
+
+            dmodpsub1 = d % (p - 1);
+            dmodqsub1 = d % (q - 1);
+        }
+
+        public BigInteger EncryptInteger(BigInteger C){
+            BigInteger mp = BigInteger.ModPow(C, dmodpsub1, p);
+            BigInteger mq = BigInteger.ModPow(C, dmodqsub1, q);
+
+            BigInteger encrypted = Ariphmetic.ChineseRemainderTheorem(new BigInteger[] { mp, mq }, new BigInteger[] { p, q });
+            
+            return encrypted;
+        }
+
+        public static BigInteger DecryptInteger(BigInteger C, BigInteger E, BigInteger N)
+        {
+            BigInteger m = BigInteger.ModPow(C, E, N);
+            return m;
+        }
+
+        public static BigInteger ToInteger(string text){
+            int strBase = supportedSymbols.Count();
+
+            BigInteger res = 0;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (!supportedSymbols.Contains(text[i])){
+                    throw new ArgumentException("The text contains unsupported symbols");
+                }
+                res = res*strBase + (BigInteger)(supportedSymbols.IndexOf(text[i]));
+            }
+            // for (int i= text.Length-1; i>=0;--i){
+            //     Console.WriteLine("To integer symbol:" + (BigInteger)(supportedSymbols.IndexOf(text[i])));
+            // }
+            return res;
+        }
+
+        public static string ToText(BigInteger number){
+            int strBase = supportedSymbols.Count();
+            string res = "";
+            while (number > 0)
+            {
+                // Console.WriteLine("To text symbol:" + (int)(number % strBase));
+                res = supportedSymbols[(int)(number % strBase)] + res;
+                number /= strBase;
+            }
+            return res;
+        }
+
+        public static BigInteger HASH_MOD = 998244353;
+        public static BigInteger HASH_P = 16777619;
+
+        public static BigInteger ToHash(BigInteger number){
+            BigInteger res = 0;
+            while (number > 0)
+            {
+                res = (res + (HASH_P * ( number % HASH_MOD +1 )) % HASH_MOD)%HASH_MOD;
+                number /= HASH_MOD;
+            }
+            return res;
+        }
+
+        public string EncryptMessage(string message){
+            string res = "Signature: ";
+
+            BigInteger messageInteger = ToInteger(message);
+
+            string Signature = ToText(ToHash(messageInteger));
+
+            res += Signature;
+            res += "\nMessage: ";
+
+            messageInteger = EncryptInteger(messageInteger);
+
+            res += ToText(messageInteger);
+
+            return res;
+        }
+
+        public static string DecryptMessage(string message, BigInteger E, BigInteger N){
+            string Signature = message.Split('\n')[0].Split(": ")[1];
+            string Message = message.Split('\n')[1].Split(": ")[1];
+
+            BigInteger mInteger = ToInteger(Message);
+            BigInteger signatureInteger = ToInteger(Signature);
+
+            BigInteger messageInteger = DecryptInteger(mInteger, E, N);
+
+            string actualMessage = ToText(messageInteger);
+
+            string res = "";
+
+            if (ToHash(messageInteger) == signatureInteger){
+                res += "Signature is correct\n";
+                res += "Message: " + actualMessage;
+            } else {
+                res += "Signature is incorrect\n";
+                res += "Message: " + actualMessage;
+            }
+            return res;
+        }
+    }
+}
